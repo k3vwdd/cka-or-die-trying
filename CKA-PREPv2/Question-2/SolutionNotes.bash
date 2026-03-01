6cda325844a34076475ce652229c46b5f8f07f1d
k3vwdd <kevwdiaz@outlook.com>
2026-03-01 13:17:52 -0500

    update: question2 update on how to search helm for a new repo using all cli

--- CKA-PREPv2/Question-2/SolutionNotes.bash
+++ CKA-PREPv2/Question-2/SolutionNotes.bash
@@ -1,5 +1,8 @@
 kubectl create namespace minio
 
+*search the hub to get the helm repo name:
+
+helm search hub minio-operator -o json | jq
 helm repo add minio https://operator.min.io
 helm repo update
 helm -n minio install minio-operator minio/operator
--- tf/servers.tf
+++ tf/servers.tf
@@ -11,8 +11,13 @@ resource "aws_instance" "my_kube_cluster" {
   subnet_id                   = data.aws_subnets.default.ids[0]
   vpc_security_group_ids      = each.value.security_group_type == "jumpbox" ? [aws_security_group.jumpbox_sg.id] : [aws_security_group.kubernetes_sg.id]
   associate_public_ip_address = each.value.security_group_type == "jumpbox" ? true : false
-  user_data                   = each.value.user_data_type == "jumpbox" ? file("../scripts/web_server_user_data_script.sh") : null
-  key_name                    = aws_key_pair.cka_cluster_key.key_name
+  iam_instance_profile        = aws_iam_instance_profile.kube_cluster_profile.name
+  user_data = each.value.user_data_type == "jumpbox" ? templatefile("../scripts/jumpbox_user_data.sh", {
+    s3_bucket = aws_s3_bucket.ssh_key_distribution.id
+    }) : templatefile("../scripts/kube_node_user_data.sh", {
+    s3_bucket = aws_s3_bucket.ssh_key_distribution.id
+  })
+  key_name = aws_key_pair.cka_cluster_key.key_name
   tags = merge(local.common_tags, each.value.tags, {
     ProvisionedBy = "Terraform and k3vwd : )"
   })
